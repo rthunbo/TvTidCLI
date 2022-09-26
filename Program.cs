be55ee3a0bfb959b6a8e46fc6fb8b96bb53a8b9d
Rolf Thunbo <rthunbo@gmail.com>
2022-09-26 14:43:24 +0200

    date formatting refac

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ class EntryPoint
 
             var programTitlesList = programSummariesList.Select(x => 
                 string.Format("{0,-15}", channels.First(c => c.Id == x.Id).Title)
-                + string.Join("", x.Programs.Select(y => string.Format(" [{0}] {1}{2}", y.Start.ToShortTimeString(), y.Title, (y.Live ? " [Live]": "")))));
+                + string.Join("", x.Programs.Select(y => string.Format(" [{0:t}] {1}{2}", y.Start, y.Title, (y.Live ? " [Live]": "")))));
 
             foreach (var programTitles in programTitlesList)
             {
